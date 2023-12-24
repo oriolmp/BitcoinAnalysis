674f8b5a357ddad5b539095bfdd0b0bff63083d2
Oriol <oriolmartinez314@gmail.com>
2023-12-24 19:08:08 +0100

    Add file to preprocess dataset

---
+++ preprocessing.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+
+from utils import *
+
+DATA_PATH = r'data\raw_data'
+PREPROCESS_DATA_PATH = r'data\preprocessed_data'
+INDICATORS = [
+    compute_variation,
+    compute_rsi,
+    compute_stochastic_oscillator,
+    compute_obv,
+    compute_atr,
+    compute_ema,
+    compute_macd,
+    compute_signal,
+    compute_chaikin
+]
+
+for file in os.listdir(DATA_PATH):
+    csv_path = os.path.join(DATA_PATH, file)
+    preprocessed_csv_path = os.path.join(PREPROCESS_DATA_PATH, file)
+
+    print(f'Processing {file}')
+
+    if not os.path.exists(PREPROCESS_DATA_PATH):
+        os.makedirs(PREPROCESS_DATA_PATH)
+
+    df = pd.read_csv(csv_path)
+
+    for indicator in INDICATORS:
+        df = indicator(df)
+    df = df.drop('Adj Close', axis=1)
+    
+    df = set_target(df)
+    df.to_csv(preprocessed_csv_path, index=False)
